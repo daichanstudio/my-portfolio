057d46d97f5b38542bd9430d417f98cabe375ee4
daichi <w.daichi2835@gmail.com%>
2021-06-04 14:36:20 +0900

    Add Dark Mode

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,24 +1,26 @@
-import {FunctionComponent} from 'react'
-import {IService} from '../type'
+import { FunctionComponent } from 'react'
+import { Service } from '../types'
+// import { motion } from 'framer-motion'
 
-const ServiceCard: FunctionComponent<{service:IService}> = ({
-  service:{Icon, about, title},
+const ServiceCard: FunctionComponent<{ service: Service }> = ({
+   service: { Icon, title, about },
 }) => {
+   //XSS attack :( on our portfolio btw, as an alternate use npm i dom purify
+   function createMarkup() {
+      return {
+         __html: about,
+      }
+   }
 
-  const createMarkup = () => {
-    return {
-      __html: about
-    }
-  }
-  return (
-    <div className='flex items-center p-2 space-x-4'>
-      <Icon className='w-12 h-12 text-green'/>
-      <div>
-        <h4 className='font-bold'>{title}</h4>
-        <p dangerouslySetInnerHTML={createMarkup()} />
+   return (
+      <div className='flex items-center p-2 space-x-4 '>
+         <Icon className='w-12 h-12 text-green' />
+         <div className=''>
+            <h6 className='font-bold'>{title}</h6>
+            <p dangerouslySetInnerHTML={createMarkup()} />
+         </div>
       </div>
-    </div>
-  )
+   )
 }
 
 export default ServiceCard
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,30 +1,38 @@
 import {AiFillGithub, AiFillTwitterCircle,AiFillYoutube} from 'react-icons/ai'
 import {GoLocation} from 'react-icons/go'
-import {GiTie, GiWingedSword} from 'react-icons/gi'
+import {GiTie} from 'react-icons/gi'
+import { useTheme } from "next-themes";
 
 const Sidebar = () => {
+  const { theme, setTheme } = useTheme();
+
+  const changeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
     <div>
       <img
         src="./images/grid9.jpg"
-        alt="user avatar"
+        alt="avatar"
         className="w-32 h-32 mx-auto rounded-full"
       />
       <h3 className="my-4 text-3xl font-medium tracking-wider font-kaushan">
         <span className="text-green">Daichi </span>
         Watanabe
       </h3>
-      <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">
+      <p className="px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200 dark:bg-black-500">
         Web Developer
       </p>
-      <a className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full" 
+            {/* Resume */}
+      <a className="flex items-center justify-center px-2 py-1 my-2 bg-gray-200 rounded-full cursor-pointer dark:bg-dark-200 dark:bg-black-500" 
          href=""
          download="name">
         <GiTie className="w-6 h-6"/>
         Download Resume
       </a>
       {/* social -icon */}
-      <div className="flex justify-around my-5 text-green-500 w-9/12 md:w-full mx-auto">
+      <div className="flex justify-around w-9/12 mx-auto my-5 text-green-500 md:w-full">
         <a href="">
           <AiFillYoutube className="w-8 h-8 cursor-pointer" />
         </a>
@@ -38,24 +46,31 @@ const Sidebar = () => {
 
       {/* address */}
       <div 
-        className="my-5 py-4 bg-gray-200"
+        className="py-4 my-5 bg-gray-200 dark:bg-dark-200 dark:bg-black-500"
         style={{marginLeft: '-1rem', marginRight: '-1rem'}}>
         <div className="flex items-center justify-center">
-          <GoLocation />
+        <GoLocation className="mr-2" />
           <span class>Japan, Tokyo</span>
         </div>
         <p className="my-2">abc@gmail.com</p>
         <p className="my-2">000-0000-0000</p>
       </div>
-      <button 
-      className="bg-gradient-to-r from-green-500 to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2 focus: outline-none"
-      
-      onClick={()=>window.open('mailto:w.daichi2835@gmail.com')}
-      >Email Me</button>
-      <button className="bg-gradient-to-r from-green-500 to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2">Toggle Theme</button>
+      <button
+        className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none"
+        onClick={() => window.open("mailto:w.daichi2835@gmail.com")}
+      >
+        Email me
+      </button>
+      <button
+        onClick={changeTheme}
+        className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 focus:outline-none hover:scale-105 "
+      >
+        {/* //TODO remove bg black */}
+        Toggle Theme
+      </button>
       
     </div>
-  )
-}
+  );
+};
 
 export default Sidebar
