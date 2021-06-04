b8e6fdc47e192460c54a4e4fe81e3bb37a58521b
daichi <w.daichi2835@gmail.com%>
2021-06-04 19:48:18 +0900

    Add Projects Page

--- type.ts
+++ type.ts
@@ -1,13 +1,24 @@
-import {IconType} from 'react-icons'
-
-export interface IService {
-  title: string,
-  about: string,
-  Icon: IconType
+import { IconType } from "react-icons";
+export interface Service {
+  Icon: IconType;
+  title: string;
+  about: string;
 }
 
-export interface ISlill {
-  name: string,
-  level: string,
-  Icon: IconType
+export interface Skill {
+  Icon: IconType;
+  name: string;
+  level: string;
 }
+
+export interface IProject {
+  name: string;
+  description: string;
+  image_path: string;
+  deployed_url: string;
+  github_url: string;
+  category: Category[];
+  key_techs: string[];
+}
+
+export type Category = "react" | "node" | "express" | "django" | "mongo";
